ab8f263fe62cbc3d218d2b01be8fbc693f31aa54
Lorenz <lorenzrenti@outlook.com>
2024-05-29 17:14:56 +0200

    Added Signup

---
+++ app/src/app/signup/signup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Validators, FormGroup, FormControl, FormBuilder, AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+}
