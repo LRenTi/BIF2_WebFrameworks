9cef7753a4a8165da61e6b52fd80b56ded2a951c
Lorenz <lorenzrenti@outlook.com>
2024-05-29 19:12:57 +0200

    Savepoint

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,22 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatCardModule} from '@angular/material/card';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([])
+    ReactiveFormsModule,
+    RouterModule.forRoot([]),
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- app/src/app/signup/signup.component.ts
+++ app/src/app/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { EmptyExpr } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { Validators, FormGroup, FormControl, FormBuilder, AbstractControl } from '@angular/forms';
 
@@ -7,5 +8,39 @@ import { Validators, FormGroup, FormControl, FormBuilder, AbstractControl } from
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+email: string = '';
+password: string = '';
+confirmPassword: string = '';
+company: string = 'FH Technikum Wien';
+street: string = '';
+city: string = '';
+zip: string = '';
+
+constructor(private fb: FormBuilder) { }
+
+ngOnInit(): void { }
+
+signupForm: FormGroup = this.fb.group({
+  email: new FormControl(this.email, [
+    Validators.required,
+    Validators.email,
+    Validators.minLength(4)
+  ]),
+  password: new FormControl(''),
+  confirmPassword: new FormControl(''),
+  company: new FormControl(this.company),
+  street: new FormControl(this.street),
+  city: new FormControl(this.city),
+  zip: new FormControl(this.zip)
+});
+
+register() {
+  console.log(this.signupForm.value);
+}
+
+get form(): { [key: string]: AbstractControl; } {
+  return this.signupForm.controls;
+}
 
 }
+
